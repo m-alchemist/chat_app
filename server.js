db09cbaa84c47197069bea5bac8702f181379b06
m-alchemist <alkhamis@email.arizona.edu>
2017-05-20 15:37:13 -0700

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http=require('http');
 const app = express()
-const port=8080;
+var port= process.env.PORT || 8080;
 const path=require('path');
 const bodyParser=require('body-parser');
 const webpack=require('webpack');
