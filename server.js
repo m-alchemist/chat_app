dcd937d81c294559cb8591a353914d81fe04252d
m-alchemist <alkhamis@email.arizona.edu>
2017-05-20 14:23:15 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http=require('http');
+const app = express()
+const port=8080;
+const path=require('path');
+const bodyParser=require('body-parser');
+const webpack=require('webpack');
+const webpackDevMiddleware=require('webpack-dev-middleware');
+const webpackConfig=require('./webpack.config')
+const server=http.createServer(app);
+const socketIo= require('socket.io')
+const io=socketIo(server);
+
+
+app.use(express.static(__dirname));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'index.html'))
+});
+
+  app.use(bodyParser.urlencoded({extends:false}));
+
+io.on('connection',socket=>{
+  socket.on('message',body=>{
+    socket.broadcast.emit('message',{
+      body,
+      from:  socket.id.slice(8)
+    })
+  })
+})
+
+// configure your socket.io instance here
+
+server.listen(port);
+console.log("server stated on port ", port)
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
-
+import io from 'socket.io-client';
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={messages:[]}
+  }
+  componentDidMount(){
+    this.socket=io('/');
+    this.socket.on('message',message=>{
+      this.setState({messages:[message, ...this.state.messages]})
+
+    } )
+  }
+  handleSubmit=(event)=>{
+    const body=event.target.value;
+    if(event.keyCode==13 && body){
+      const message={
+        body,
+        from:'Me'
+      }
+      this.setState({messages:[message, ...this.state.messages]})
+      console.log('1');
+      this.socket.emit('message',body);
+        console.log('2');
+      event.target.value='';
+    }
+  }
   render() {
+    const messages=this.state.messages.map((message,index)=>{
+      return (<li key={index}><b>{message.from}:</b> {message.body} </li>)
+    })
     return (
-      <div>React simple starter</div>
+      <div>
+      <input type='text' placeholder='enter a message...'
+      onKeyUp={this.handleSubmit}/>
+      {messages}
+
+      </div>
     );
   }
 }
